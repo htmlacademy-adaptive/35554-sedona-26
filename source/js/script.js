82e3f2587099b6093de9147ece783228c5f9da1e
Nanammii <kaddia@gmail.com>
2022-08-23 14:52:20 +0700

    added scss media form.html

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,22 @@
-let navMain = document.querySelector('.main-navigation');
+let navMainVisible = document.querySelector('#visible');
+let navMainInvisible = document.querySelector('#invisible');
+let navMainList = document.querySelector('.main-navigation__list');
 let navToggle = document.querySelector('.main-navigation__toggle');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-navigation--closed')) {
-    navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--opened');
+  if (navMainVisible.classList.contains('main-navigation__icon--visible')) {
+    navMainVisible.classList.remove('main-navigation__icon--visible');
+    navMainVisible.classList.add('main-navigation__icon--invisible');
   } else {
-    navMain.classList.add('main-navigation--closed');
-    navMain.classList.remove('main-navigation--opened')
+    navMainVisible.classList.add('main-navigation__icon--visible');
+    navMainVisible.classList.remove('main-navigation__icon--invisible')
   }
+  if (navMainInvisible.classList.contains('main-navigation__icon--visible')) {
+    navMainInvisible.classList.remove('main-navigation__icon--visible');
+    navMainInvisible.classList.add('main-navigation__icon--invisible');
+  } else {
+    navMainInvisible.classList.add('main-navigation__icon--visible');
+    navMainInvisible.classList.remove('main-navigation__icon--invisible')
+  }
+  navMainList.classList.toggle('main-navigation__list--visible')
 });
