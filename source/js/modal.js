4a92910fd8157588b0e8b25b247a14204adaf4e5
Nanammii <kaddia@gmail.com>
2022-08-24 23:36:28 +0700

    added scss modal

---
+++ source/js/modal.js
@@ -0,0 +1,12 @@
+let modal = document.querySelectorAll('.modal');
+let modalButton = document.querySelector('.modal__button');
+
+modalButton.addEventListener('click', function () {
+  if (modal.classList.contains('modal-close')) {
+    modal.classList.remove('modal-close');
+  } else {
+    modal.classList.add('modal-close');
+  }
+})
+
+
